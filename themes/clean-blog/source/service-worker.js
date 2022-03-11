633cbb4e2a41fff918aaf32ef6da35a61a5badf4
John Turner <johneturner@gmail.com>
2022-03-11 02:53:09 -0500

    Update service-worker.js

--- themes/clean-blog/source/service-worker.js
+++ themes/clean-blog/source/service-worker.js
@@ -1,14 +1,24 @@
-self.addEventListener("install", function (e) {
-  self.skipWaiting();
-});
+// from: https://dev.to/paco_ita/service-workers-and-caching-strategies-explained-step-3-m4f
 
-self.addEventListener("activate", function (e) {
-  self.registration
-    .unregister()
-    .then(function () {
-      return self.clients.matchAll();
-    })
-    .then(function (clients) {
-      clients.forEach((client) => client.navigate(client.url));
-    });
-});
+caches.keys().then((keys) =>
+  Promise.all(keys.map(async (key) => await caches.delete(key)))
+    .then(async () => await registration.unregister())
+    .catch((err) => console.error("Something went wrong: ", err))
+);
+
+// OLD CODE BELOW
+
+// self.addEventListener("install", function (e) {
+//   self.skipWaiting();
+// });
+
+// self.addEventListener("activate", function (e) {
+//   self.registration
+//     .unregister()
+//     .then(function () {
+//       return self.clients.matchAll();
+//     })
+//     .then(function (clients) {
+//       clients.forEach((client) => client.navigate(client.url));
+//     });
+// });
